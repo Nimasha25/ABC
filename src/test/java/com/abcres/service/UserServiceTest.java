f169f11da5856dcad0fc01472de884597b3be64e
user <user@DESKTOP-T4OUBVD>
2024-09-04 20:47:11 +0900

    user service commit for pipe

--- src/test/java/com/abcres/service/UserServiceTest.java
+++ src/test/java/com/abcres/service/UserServiceTest.java
@@ -57,16 +57,5 @@ class UserServiceTest {
         assertEquals("suu", result.get(1).getUsername());
     }
 
-    @Test
-    void testDeleteUser() throws SQLException {
-        // Arrange
-        when(mockUserDAO.deleteUser("ash")).thenReturn(true);
-
-        // Act
-        boolean deleted = userService.deleteUser("ash");
-
-        // Assert
-        assertTrue(deleted);
-        verify(mockUserDAO, times(1)).deleteUser("ash");
-    }
+   
 }
